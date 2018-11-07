6d67fa2346205d39c9aa0c4b0b56f939755d8cc0
Jean Lucas Monte Carvalho <jmontecarvalho@gmail.com>
2018-11-07 21:55:36 -0200

    added questions

--- src/main/java/com/nutribot/nutribot/NutriBootstrap.java
+++ src/main/java/com/nutribot/nutribot/NutriBootstrap.java
@@ -47,13 +47,13 @@ public class NutriBootstrap {
 
         questions.add(new Question(3, new String[]{"Como fazer a preparação de álcool"}
                 , new String[]{"Para se fazer a preparação do álcool 70% é necessário" +
-                "\n1. Pegar um recipiente com capacidade de 1 litro; \n" +
+                "\n1. Um recipiente com capacidade de 1 litro; \n" +
                 "2.  Adicionar 750 ml de álcool com densidade de 96% e 97%;\n" +
                 "3.  Adicionar 250 ml de água destilada, e misturar;\n" +
                 "4.  Distribuir em borrifadores identificados. \n"}));
 
-        questions.add(new Question(4, new String[]{"Quando as mão devem ser higienizadas "}
-                , new String[]{"As mão devem ser bem higienizadas quando: \n" +
+        questions.add(new Question(4, new String[]{"Quando as mãos devem ser higienizadas "}
+                , new String[]{"As mãos devem ser bem higienizadas quando: \n" +
                 "1. Utilizar os sanitários ou vestiários;\n" +
                 "2. Iniciar, interromper ou trocar de atividade;\n" +
                 "3. Após manipular alimentos crus ou não higienizados;\n" +
@@ -80,7 +80,7 @@ public class NutriBootstrap {
                 "Manipular dinheiro.\n"}));
 
         questions.add(new Question(6, new String[]{"como preparar solução clorada"}
-                , new String[]{"Para o preparo da solução clorada é necessário: " +
+                , new String[]{"Para o preparo da solução clorada é necessário: \n" +
                 "1. Separar um recipiente com capacidade de 1 litro;\n" +
                 "2. Adicionar 1 litro de água tratada;\n" +
                 "3. Adicionar 1 colher de sopa de água sanitária.\n"}));
@@ -196,6 +196,54 @@ public class NutriBootstrap {
                 "4. Coar em peneira fina 3 vezes;\n" +
                 "5. Acondicionar na porção superior da geladeira; \n" +
                 "\n"}));
+
+
+        questions.add(new Question(22, new String[]{"Quantas calorias tem a dieta?", "Quantas kcal tem a dieta?"}
+                , new String[]{"A dieta possui 1931kcal. "}));
+
+
+        questions.add(new Question(23, new String[]{"Qual a distribuição dos macronutrientes?"}
+                , new String[]{"25% de PTN; 40% LIP (TCM19%; óleo de peixe 19%; 5% lecitina de soja); 35% de CHO ( Maltodextrina 90% e amido de milho 10%)\n "}));
+
+        questions.add(new Question(24, new String[]{"Qual critério de escolha para a fórmula? "}
+                , new String[]{"Fórmula parcialmente desidratada, pois a sonda do paciente encontra-se na região pós-pilórica. Hipoglicêmica, para controle da glicemia do paciente acamado. (acho que tá faltando informação ai cê completa, PORFA)\n"}));
+
+        questions.add(new Question(25, new String[]{"O que fazer em casos de diarreia? "}
+                , new String[]{"• Caso  receba sucos laxativos (laranja, abacaxi e manga) substituí-los por sucos constipantes (caju, limão, goiaba e maçã, coados)\n" +
+                "    • Caso a dieta contenha leite substituí-la por leite com teor reduzido de lactose ou bebida à base de soja. Não acrescentar à dieta alimentos ricos em fibras com canela, farelos e aveia. \n" +
+                "    • Administrar a dieta lentamente\n" +
+                "    • Verificar a temperatura em que a dieta está sendo administrada. \n" +
+                "    • Verificar as técnicas de preparo e conservação da dieta\n" +
+                "    • Caso a diarreia não melhore com as medidas acima, procure a nutricionista responsável.\n"}));
+
+        questions.add(new Question(26, new String[]{" O que fazer em caso de vômito?\n "}
+                , new String[]{"Suspenda a dieta até o próximo horário de administração; \n" +
+                "    • Verifique a posição do paciente (deve sempre estar posicionado entre 45° e 60°); \n" +
+                "    • Administre a dieta lentamente e, se necessário, diminua o volume de infusão; No caso de sonda enteral com acesso pelo nariz (nasoentérica), confira se a marcação do posicionamento da sonda está adequada, ou seja, próxima à narina. \n" +
+                "    • Caso a sonda esteja muito exteriorizada, suspenda o próximo horário da dieta e entre em contato com o enfermeiro responsável; \n" +
+                "    • Caso os vômitos não cessem, procure seu Centro de Saúde ou serviço de Pronto Atendimento. \n"}));
+
+        questions.add(new Question(27, new String[]{"O que fazer em caso de constipação?"}
+                , new String[]{"Ofertar sucos laxativos (laranja, mamão, manga, ameixa).\n" +
+                "    • Avaliar se a oferta hídrica diária (água da dieta + água para higienizar a sonda 11 + água para diluir medicamentos) está de acordo com o prescrito. \n" +
+                "    • Usar diariamente no preparo da dieta aveia e canela em pó (fontes de fibras).\n" +
+                "    • Incentivar a movimentação do paciente saindo da posição deitada para sentado ou de pé. \n" +
+                "    • Massagens no abdômen podem auxiliar no processo de evacuação. \n" +
+                "    • Caso o usuário não apresente evacuação por até 5 dias, procure o Centro de Saúde ou serviço de Pronto Atendimento\n"}));
+
+
+        questions.add(new Question(28, new String[]{"O que devo comprar para dieta artesanal?","Lista de compras"}
+                , new String[]{"Vou lhe enviar a lista de compras, só um momento."}, new String[]{"listacompras.png"}));
+
+
+        questions.add(new Question(29, new String[]{"Quais as interações entre remédios e nutrientes ?", "Quais as interações entre fármacos e nutrientes?"}
+                , new String[]{"1. Ácido acetilsalicílico (AAS) interação com álcool que pode causar dano na mucosa gastrointestinal e pode prolongar tempo de sangramento.  \n" +
+                "2. Atorvastatina: interação com suco de grapefruit que possui componente que impede a ação da CYP3A4 pode do aumentar as concentrações do fármaco. \n" +
+                "3. Levotiroxina: pode interagir com farinha de soja, cereais de semente de algodão, nozes e dieta à base de fibras, podendo diminuir a absorção do fármaco. "}));
+
+
+        questions.add(new Question(30, new String[]{"Qual a quantidade que devo usar de cada ingrediente para dieta artesanal ? "}
+                , new String[]{"As quantidades de cada ingrediente serão iguais as da imagem a seguir:"}, new String[]{"qnt.png"}));
 
         return questions;
     }
